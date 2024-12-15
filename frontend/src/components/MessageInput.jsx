36e31ad9ddcec3742aff2a403aa72a1d0187c291
zhullng <joaozhu10@gmail.com>
2024-12-15 12:54:05 +0000

    a

--- frontend/src/components/MessageInput.jsx
+++ frontend/src/components/MessageInput.jsx
@@ -73,7 +73,7 @@ const MessageInput = () => {
         <div className="flex-1 flex gap-2">
           <input
             type="text"
-            className="w-full input input-bordered rounded-lg input-sm sm:input-md"
+            className="w-full input input-bordered rounded-lg input-md"
             placeholder="Type a message..."
             value={text}
             onChange={(e) => setText(e.target.value)}
