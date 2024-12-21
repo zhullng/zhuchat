312fc8d29bf105c22d1926d4b6aad01f72c7e664
zhullng <joaozhu10@gmail.com>
2024-12-21 22:56:23 +0000

    a

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -1,28 +1,37 @@
 import jwt from "jsonwebtoken";
 import User from "../models/user.model.js";
 
+// Middleware de proteção de rota
 export const protectRoute = async (req, res, next) => {
   try {
+    // Obter o token do cookie
     const token = req.cookies.jwt;
 
+    // Se não houver token, retornar erro de autorização
     if (!token) {
       return res.status(401).json({ message: "Unauthorized - No Token Provided" });
     }
 
+    // Verificar e decodificar o token usando o segredo JWT
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
+    // Se o token não for válido, retornar erro de autorização
     if (!decoded) {
       return res.status(401).json({ message: "Unauthorized - Invalid Token" });
     }
 
+    // Buscar o usuário no banco de dados usando o ID do token
     const user = await User.findById(decoded.userId).select("-password");
 
+    // Se o usuário não for encontrado, retornar erro
     if (!user) {
       return res.status(404).json({ message: "User not found" });
     }
 
+    // Adicionar o usuário à requisição para que possa ser acessado em rotas subsequentes
     req.user = user;
 
+    // Chamar o próximo middleware ou rota
     next();
   } catch (error) {
     console.log("Error in protectRoute middleware: ", error.message);
