a30e1c51b33f2fe6fb8be4d6ab68f397026938db
Zhu <joaozhu10@gmail.com>
2025-01-27 23:12:00 +0000

    a

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-import './i18n'; // Importa o arquivo de configuração do i18next
+import './lib/i18next'; // Importa o arquivo de configuração do i18next
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
