37b621ae45202245c8ea5c6afce7299f9b6113fd
Zhu <joaozhu10@gmail.com>
2025-01-27 23:56:17 +0000

    a

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,4 +1,5 @@
 import { useChatStore } from "../store/useChatStore";
+
 import Sidebar from "../components/Sidebar";
 import NoChatSelected from "../components/NoChatSelected";
 import ChatContainer from "../components/ChatContainer";
@@ -11,20 +12,13 @@ const HomePage = () => {
       <div className="flex items-center justify-center pt-20 px-4">
         <div className="bg-base-100 rounded-lg shadow-cl w-full max-w-6xl h-[calc(100vh-8rem)]">
           <div className="flex h-full rounded-lg overflow-hidden">
-            {/* Sidebar */}
-            <div className={`${selectedUser ? 'hidden' : 'block'} lg:block`}>
-              <Sidebar />
-            </div>
+            <Sidebar />
 
-            {/* ChatContainer */}
-            <div className="flex-1">
-              {selectedUser ? <ChatContainer /> : <NoChatSelected />}
-            </div>
+            {!selectedUser ? <NoChatSelected /> : <ChatContainer />}
           </div>
         </div>
       </div>
     </div>
   );
 };
-
 export default HomePage;
