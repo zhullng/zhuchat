2f6d1302dcf402d599ae84274567bc6b63489920
Zhu <joaozhu10@gmail.com>
2025-02-22 23:40:00 +0000

    a

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -2,6 +2,7 @@ import { useChatStore } from "../store/useChatStore";
 import Sidebar from "../components/Sidebar";
 import NoChatSelected from "../components/NoChatSelected";
 import ChatContainer from "../components/ChatContainer";
+import AIChat from "./AIChat";
 
 const HomePage = () => {
   const { selectedUser } = useChatStore();
