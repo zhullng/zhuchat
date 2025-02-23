a6e346022fc53574e07fb113041d6f4566eef434
Zhu <joaozhu10@gmail.com>
2025-02-23 13:06:54 +0000

    a

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -11,8 +11,8 @@ const HomePage = () => {
   return (
     <div className="h-screen bg-base-200">
       <div className="flex items-center justify-center pt-20 px-4">
-        <div className="bg-base-100 rounded-lg shadow-cl w-full h-[calc(100vh-8rem)]">
-          <div className="flex h-full rounded-lg overflow-hidden">
+        <div className="w-full h-[calc(100vh-8rem)]">
+          <div className="flex h-full">
             <Sidebar />
 
             {/* Se for AI, exibe o AIChat, caso contrário, o ChatContainer */}
