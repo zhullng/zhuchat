6cb83198d07adb755455127c53a7bcc3ac00d97a
Zhu <joaozhu10@gmail.com>
2025-02-23 15:16:42 +0000

    aaa

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -25,10 +25,9 @@ const LoginPage = () => {
           <div className="text-center mb-8">
             <div className="flex flex-col items-center gap-2 group">
               <div
-                className="w-12 h-12 rounded-xl bg-primary/10 flex items-center justify-center group-hover:bg-primary/20
-              transition-colors"
+                className="w-12 h-12 rounded-xl flex items-center justify-center"
               >
-                <img src="/logoZhuChat.png" alt="Logo" className="w-6 h-6" />
+                <img src="/logoZhuChat.png" alt="Logo" className="w-12 h-12" />
               </div>
               <h1 className="text-2xl font-bold mt-2">Welcome Back</h1>
               <p className="text-base-content/60">Sign in to your account</p>
--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -39,8 +39,10 @@ const SignUpPage = () => {
         <div className="w-full max-w-md space-y-8">
           <div className="text-center mb-8">
             <div className="flex flex-col items-center gap-2 group">
-              <div className="size-12 rounded-xl bg-primary/10 flex items-center justify-center group-hover:bg-primary/20 transition-colors">
-                <img src="/logoZhuChat.png" alt="Logo" className="w-6 h-6" />
+            <div
+                className="w-12 h-12 rounded-xl flex items-center justify-center"
+              >
+                <img src="/logoZhuChat.png" alt="Logo" className="w-12 h-12" />
               </div>
               <h1 className="text-2xl font-bold mt-2">Create Account</h1>
               <p className="text-base-content/60">Get started with your free account</p>
