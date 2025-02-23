645fa43176355c624862128736c0bce8c8e41d10
Zhu <joaozhu10@gmail.com>
2025-02-23 17:30:12 +0000

    ios

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -44,49 +44,47 @@ const ChatContainer = () => {
   }
 
   return (
-    <div className="flex-1 flex flex-col overflow-auto pb-5 h-full">
-      <ChatHeader />
-
-      <div className="flex-1 overflow-y-auto p-4 space-y-4">
-        {messages.map((message) => (
-          <div
-            key={message._id}
-            className={`chat ${message.senderId === authUser._id ? "chat-end" : "chat-start"}`}
-            ref={messageEndRef}
-          >
-            <div className="chat-image avatar">
-              <div className="size-10 rounded-full border">
-                <img
-                  src={
-                    message.senderId === authUser._id
-                      ? authUser.profilePic || "/avatar.png"
-                      : selectedUser.profilePic || "/avatar.png"
-                  }
-                  alt="profile pic"
-                />
-              </div>
-            </div>
-            <div className="chat-header mb-1">
-              <time className="text-xs opacity-50 ml-1">
-                {formatMessageTime(message.createdAt)}
-              </time>
-            </div>
-            <div className="chat-bubble flex flex-col">
-              {message.image && (
-                <img
-                  src={message.image}
-                  alt="Attachment"
-                  className="sm:max-w-[200px] rounded-md mb-2"
-                />
-              )}
-              {message.text && <p>{message.text}</p>}
-            </div>
+<div className="min-safe-h-screen flex-1 flex flex-col overflow-auto pb-5">
+  <ChatHeader />
+  <div className="flex-1 overflow-y-auto p-4 space-y-4">
+    {messages.map((message) => (
+      <div
+        key={message._id}
+        className={`chat ${message.senderId === authUser._id ? "chat-end" : "chat-start"}`}
+      >
+        <div className="chat-image avatar">
+          <div className="size-10 rounded-full border">
+            <img
+              src={
+                message.senderId === authUser._id
+                  ? authUser.profilePic || "/avatar.png"
+                  : selectedUser.profilePic || "/avatar.png"
+              }
+              alt="profile pic"
+            />
           </div>
-        ))}
+        </div>
+        <div className="chat-header mb-1">
+          <time className="text-xs opacity-50 ml-1">
+            {formatMessageTime(message.createdAt)}
+          </time>
+        </div>
+        <div className="chat-bubble flex flex-col">
+          {message.image && (
+            <img
+              src={message.image}
+              alt="Attachment"
+              className="sm:max-w-[200px] rounded-md mb-2"
+            />
+          )}
+          {message.text && <p>{message.text}</p>}
+        </div>
       </div>
+    ))}
+  </div>
+  <MessageInput />
+</div>
 
-        <MessageInput />
-    </div>
   );
 };
 
