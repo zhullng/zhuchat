1fcbb2191e96f9c4345355ee2c7c271dc1f72b07
Zhu <joaozhu10@gmail.com>
2025-02-23 16:27:03 +0000

    alteracao

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ import LoginPage from "./pages/LoginPage";
 import SettingsPage from "./pages/SettingsPage"; 
 import ProfilePage from "./pages/ProfilePage"; 
 
-import { Routes, Route, Navigate, useLocation } from "react-router-dom"; // Adicionei useLocation
+import { Routes, Route, Navigate, useLocation } from "react-router-dom"; 
 import { useAuthStore } from "./store/useAuthStore"; 
 import { useThemeStore } from "./store/useThemeStore"; 
 import { useEffect } from "react"; 
@@ -35,42 +35,25 @@ const App = () => {
     );  
   }
 
-  // Verifica se a rota atual é login ou signup
   const isLoginOrSignupPage = location.pathname === "/login" || location.pathname === "/signup";
 
   return (
-    <div style={{ overflow: "hidden" }} data-theme={theme}> {/* Desabilita o scroll aqui */}
+    <div style={{ overflow: "hidden" }} data-theme={theme}>
       {/* Renderiza a Navbar apenas se não for a página de login ou signup */}
       {!isLoginOrSignupPage && <Navbar />}
 
       <Routes>
-        <Route
-          path="/"
-          element={authUser ? <HomePage /> : <Navigate to="/login" />}
-        />
-        <Route
-          path="/signup"
-          element={!authUser ? <SignUpPage /> : <Navigate to="/" />}
-        />
-        <Route
-          path="/login"
-          element={!authUser ? <LoginPage /> : <Navigate to="/" />}
-        />
+        <Route path="/" element={authUser ? <HomePage /> : <Navigate to="/login" />} />
+        <Route path="/signup" element={!authUser ? <SignUpPage /> : <Navigate to="/" />} />
+        <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
         <Route path="/settings" element={<SettingsPage />} />
-        <Route
-          path="/profile"
-          element={authUser ? <ProfilePage /> : <Navigate to="/login" />}
-        />
-        <Route
-          path="/chat"
-          element={authUser ? <AIChat /> : <Navigate to="/login" />}
-        />
+        <Route path="/profile" element={authUser ? <ProfilePage /> : <Navigate to="/login" />} />
+        <Route path="/chat" element={authUser ? <AIChat /> : <Navigate to="/login" />} />
       </Routes>
 
       <Toaster /> 
     </div>
   );
 };
-
 
 export default App;
