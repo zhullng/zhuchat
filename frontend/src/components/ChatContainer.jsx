c6314d0057814fb539ae598dcf36fcbc13895c00
Zhu <joaozhu10@gmail.com>
2025-02-23 18:28:09 +0000

    a

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,11 +1,10 @@
 import { useChatStore } from "../store/useChatStore";
-import { useEffect, useRef } from "react";
-
+import { useEffect, useRef, useState } from "react";
 import ChatHeader from "./ChatHeader";
 import MessageInput from "./MessageInput";
 import MessageSkeleton from "./skeletons/MessageSkeleton";
 import { useAuthStore } from "../store/useAuthStore";
-import { formatMessageTime } from "../lib/utils";
+import { formatMessageTime, formatMessageDate } from "../lib/utils";
 
 const ChatContainer = () => {
   const {
@@ -18,6 +17,7 @@ const ChatContainer = () => {
   } = useChatStore();
   const { authUser } = useAuthStore();
   const messageEndRef = useRef(null);
+  const [lastMessageDate, setLastMessageDate] = useState(null);
 
   useEffect(() => {
     getMessages(selectedUser._id);
@@ -27,11 +27,24 @@ const ChatContainer = () => {
   }, [selectedUser._id, getMessages, subscribeToMessages, unsubscribeFromMessages]);
 
   useEffect(() => {
-    // Só faz o scroll se houver mensagens e se a referência do final da mensagem estiver acessível
     if (messageEndRef.current && messages.length > 0) {
       messageEndRef.current.scrollIntoView({ behavior: "smooth" });
     }
   }, [messages]);
+
+  const renderMessageDate = (messageDate) => {
+    const formattedDate = formatMessageDate(messageDate); // Dia e mês
+    const isSameDay = lastMessageDate === formattedDate;
+    if (!isSameDay) {
+      setLastMessageDate(formattedDate);
+      return (
+        <div className="text-center text-xs text-gray-500 my-2">
+          <span>{formattedDate}</span>
+        </div>
+      );
+    }
+    return null;
+  };
 
   if (isMessagesLoading) {
     return (
@@ -47,41 +60,48 @@ const ChatContainer = () => {
     <div className="h-screen supports-[height:100cqh]:h-[100cqh] supports-[height:100svh]:h-[100svh] flex-1 flex flex-col overflow-auto pb-5">
       <ChatHeader />
       <div className="flex-1 overflow-y-auto p-4 space-y-4">
-        {messages.map((message) => (
-          <div
-            key={message._id}
-            className={`chat ${message.senderId === authUser._id ? "chat-end" : "chat-start"}`}
-          >
-            <div className="chat-image avatar">
-              <div className="size-10 rounded-full border">
-                <img
-                  src={
-                    message.senderId === authUser._id
-                      ? authUser.profilePic || "/avatar.png"
-                      : selectedUser.profilePic || "/avatar.png"
-                  }
-                  alt="profile pic"
-                />
+        {messages.map((message, index) => {
+          const messageDate = new Date(message.createdAt);
+          const dateSection = renderMessageDate(messageDate);
+
+          return (
+            <div key={message._id}>
+              {dateSection}
+
+              <div
+                className={`chat ${message.senderId === authUser._id ? "chat-end" : "chat-start"}`}
+              >
+                <div className="chat-image avatar">
+                  <div className="size-10 rounded-full border">
+                    <img
+                      src={
+                        message.senderId === authUser._id
+                          ? authUser.profilePic || "/avatar.png"
+                          : selectedUser.profilePic || "/avatar.png"
+                      }
+                      alt="profile pic"
+                    />
+                  </div>
+                </div>
+                <div className="chat-header mb-1">
+                  <time className="text-xs opacity-50 ml-1">
+                    {formatMessageTime(message.createdAt)} {/* Exibe só a hora */}
+                  </time>
+                </div>
+                <div className="chat-bubble flex flex-col">
+                  {message.image && (
+                    <img
+                      src={message.image}
+                      alt="Attachment"
+                      className="sm:max-w-[200px] rounded-md mb-2"
+                    />
+                  )}
+                  {message.text && <p>{message.text}</p>}
+                </div>
               </div>
             </div>
-            <div className="chat-header mb-1">
-              <time className="text-xs opacity-50 ml-1">
-                {formatMessageTime(message.createdAt)}
-              </time>
-            </div>
-            <div className="chat-bubble flex flex-col">
-              {message.image && (
-                <img
-                  src={message.image}
-                  alt="Attachment"
-                  className="sm:max-w-[200px] rounded-md mb-2"
-                />
-              )}
-              {message.text && <p>{message.text}</p>}
-            </div>
-          </div>
-        ))}
-        {/* Referência para a última mensagem */}
+          );
+        })}
         <div ref={messageEndRef} />
       </div>
       <MessageInput />
