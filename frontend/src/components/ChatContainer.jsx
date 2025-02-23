e097e5e9f04c447c730faefd9ea2c6fff2924473
Zhu <joaozhu10@gmail.com>
2025-02-23 15:20:12 +0000

    a

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -3,6 +3,7 @@ import { useEffect, useRef } from "react";
 
 import ChatHeader from "./ChatHeader";
 import MessageInput from "./MessageInput";
+import MessageSkeleton from "./skeletons/MessageSkeleton";
 import { useAuthStore } from "../store/useAuthStore";
 import { formatMessageTime } from "../lib/utils";
 
@@ -36,6 +37,7 @@ const ChatContainer = () => {
     return (
       <div className="flex-1 flex flex-col overflow-auto">
         <ChatHeader />
+        <MessageSkeleton />
         <MessageInput />
       </div>
     );
