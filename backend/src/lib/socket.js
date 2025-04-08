41514c65787c044c2f3c6f89873d6e7dbedf6416
Zhu <joaozhu10@gmail.com>
2025-04-08 08:32:59 +0100

    frontend url

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -12,7 +12,7 @@ const server = http.createServer(app);
 // Cria uma instância do Server do Socket.IO, associada ao servidor HTTP com configurações otimizadas
 const io = new Server(server, {
   cors: {
-    origin: process.env.CLIENT_URL || ["http://localhost:5173"],
+    origin: process.env.FRONTEND_URL || ["http://localhost:5173"],
     credentials: true
   },
   // Aumentar o tamanho máximo do pacote para suportar imagens
