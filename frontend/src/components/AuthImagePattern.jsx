6923b465de485fa6b4f73a37c7ec420cd7e438e5
Zhu <joaozhu10@gmail.com>
2025-04-23 19:41:08 +0100

    pattern 2.0

--- frontend/src/components/AuthImagePattern.jsx
+++ frontend/src/components/AuthImagePattern.jsx
@@ -4,22 +4,24 @@ const AuthImagePattern = ({ title, subtitle }) => {
   return (
     <div className="hidden lg:flex flex-col items-center justify-center bg-base-200 p-12">
       <div className="max-w-md text-center">
-        {/* Pattern design with strategically placed squares */}
-        <div className="relative h-64 w-64 mx-auto mb-12">
-          {/* Center square */}
-          <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-24 h-24 bg-primary/15 rounded-2xl animate-pulse" />
+        {/* Asymmetric pattern with squares of different sizes */}
+        <div className="relative h-80 w-80 mx-auto mb-12">
+          {/* Small square top left */}
+          <div className="absolute left-1/4 top-0 w-12 h-12 bg-primary/10 rounded-lg" />
           
-          {/* Corner squares */}
-          <div className="absolute left-0 top-0 w-20 h-20 bg-primary/15 rounded-2xl animate-pulse" />
-          <div className="absolute right-0 top-0 w-20 h-20 bg-primary/15 rounded-2xl" />
-          <div className="absolute left-0 bottom-0 w-20 h-20 bg-primary/15 rounded-2xl" />
-          <div className="absolute right-0 bottom-0 w-20 h-20 bg-primary/15 rounded-2xl animate-pulse" />
+          {/* Medium squares top right */}
+          <div className="absolute left-1/2 top-0 w-20 h-20 bg-primary/15 rounded-xl" />
+          <div className="absolute left-3/4 top-0 w-20 h-20 bg-primary/15 rounded-xl animate-pulse" />
           
-          {/* Middle edge squares */}
-          <div className="absolute left-1/2 top-0 -translate-x-1/2 w-20 h-20 bg-primary/15 rounded-2xl" />
-          <div className="absolute left-0 top-1/2 -translate-y-1/2 w-20 h-20 bg-primary/15 rounded-2xl" />
-          <div className="absolute right-0 top-1/2 -translate-y-1/2 w-20 h-20 bg-primary/15 rounded-2xl animate-pulse" />
-          <div className="absolute left-1/2 bottom-0 -translate-x-1/2 w-20 h-20 bg-primary/15 rounded-2xl animate-pulse" />
+          {/* Large squares center-left column */}
+          <div className="absolute left-1/4 top-1/3 w-24 h-24 bg-primary/15 rounded-2xl animate-pulse" />
+          <div className="absolute left-1/4 top-2/3 w-24 h-24 bg-primary/15 rounded-2xl" />
+          
+          {/* Small squares right side grid */}
+          <div className="absolute right-0 top-1/2 w-12 h-12 bg-primary/10 rounded-lg" />
+          <div className="absolute right-16 top-1/2 w-12 h-12 bg-primary/10 rounded-lg animate-pulse" />
+          <div className="absolute right-0 top-[calc(50%+3rem)] w-12 h-12 bg-primary/10 rounded-lg animate-pulse" />
+          <div className="absolute right-16 top-[calc(50%+3rem)] w-12 h-12 bg-primary/10 rounded-lg" />
         </div>
         
         <h2 className="text-2xl font-bold mb-4">{title}</h2>
