0354a5b75fa862ac9def5c27ee2e7abbdc1041cd
Zhu <joaozhu10@gmail.com>
2025-04-29 11:15:51 +0100

    2.2

--- frontend/src/components/AuthImagePattern.jsx
+++ frontend/src/components/AuthImagePattern.jsx
@@ -18,7 +18,7 @@ const AuthImagePattern = ({ title, subtitle }) => {
   return (
     <div className="hidden lg:flex items-center justify-center bg-base-200 p-12">
       <div className="max-w-md text-center">
-        <div className="grid grid-cols-3 gap-3 mb-8 ml-8">
+        <div className="grid grid-cols-3 gap-3 mb-8 ml-28">
           {[...Array(9)].map((_, i) => (
             <div
               key={i}
